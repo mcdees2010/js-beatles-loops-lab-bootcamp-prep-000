c39f7a73956e2a0d51062f895580a18cf680aa9a
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-11 01:24:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+ var arr = []
+
+  for (var i = 0; i < musicians.length; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts){
+  var i = 0
+  while(i < facts.length) {
+    console.log(`${facts[i]}!!!`)
+  }
+  i++;
+}
+
+
